5b668af285cecc8952e847603e1fa75cd163fa14
sousalmdev <leandromedeiros690@gmail.com>
2024-04-03 15:55:41 -0300

    Fix: Links for mobile menu

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -10,12 +10,8 @@ import {
 } from '@chakra-ui/react';
 import { jost } from '../layout';
 import { TbMenu } from 'react-icons/tb';
-import { BiBrain, BiHome, BiLinkExternal, BiMap, BiMenu, BiNews, BiSolidBrain, BiSolidHome, BiSolidLock, BiSolidMap, BiSolidUser, BiUser } from 'react-icons/bi';
+import { BiMenu, BiSolidLock,BiUser } from 'react-icons/bi';
 import Link from 'next/link';
-
-import { MdLink } from 'react-icons/md';
-import { SiGo } from 'react-icons/si';
-
 export const MenuUi = () => {
     return (
         <div className={`${jost.className} flex self-end text-right sm:hidden`}>
@@ -25,11 +21,11 @@ export const MenuUi = () => {
            </MenuButton>
                 <MenuList  background={'black'} textAlign={'right'}  color={'rgb(74 222 128)'}>
                     <MenuItem className='flex justify-between' icon={BiUser(Icon)}  background={'black'}>
-                   <Link passHref href="/about">About</Link> 
+                   <Link passHref href="https://leonewport.vercel.app/about">About</Link> 
                     </MenuItem>
                 
                     <MenuItem disabled className='flex justify-between' icon={BiSolidLock(Icon)}   background={'black'}>
-                    <Link passHref href="/privacy">Legal (not ready yet calm down!!!)</Link>
+                    <Link passHref href="https://leonewport.vercel.app/privacy">Legal (not ready yet calm down!!!)</Link>
                     </MenuItem>
                   
                 </MenuList>
