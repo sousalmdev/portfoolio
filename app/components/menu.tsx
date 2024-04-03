cea358a107c69a91eb0ce1dd2d3698727019597f
sousalmdev <leandromedeiros690@gmail.com>
2024-04-03 16:01:58 -0300

    Fix: Changing links

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,35 +1,51 @@
-import React from 'react';
+import React from "react";
 import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    IconButton,
-    Icon,
-    MenuDivider
-} from '@chakra-ui/react';
-import { jost } from '../layout';
-import { TbMenu } from 'react-icons/tb';
-import { BiMenu, BiSolidLock,BiUser } from 'react-icons/bi';
-import Link from 'next/link';
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Icon,
+  MenuDivider,
+} from "@chakra-ui/react";
+import { jost } from "../layout";
+import { TbMenu } from "react-icons/tb";
+import { BiMenu, BiSolidLock, BiUser } from "react-icons/bi";
+import Link from "next/link";
 export const MenuUi = () => {
-    return (
-        <div className={`${jost.className} flex self-end text-right sm:hidden`}>
-            <Menu>
-           <MenuButton className=' text-green-400'>
-           <BiMenu size={'24px'}/>
-           </MenuButton>
-                <MenuList  background={'black'} textAlign={'right'}  color={'rgb(74 222 128)'}>
-                    <MenuItem as={Link} href={"/about"} className='flex justify-between' icon={BiUser(Icon)}  background={'black'}>
-                   About
-                    </MenuItem>
-                
-                    <MenuItem disabled as={Link} href="https://leonewport.vercel.app/privacy" className='flex justify-between' icon={BiSolidLock(Icon)}   background={'black'}>
-Legal (not ready yet calm down!!!)
-                    </MenuItem>
-                  
-                </MenuList>
-            </Menu>
-        </div>
-    );
+  return (
+    <div className={`${jost.className} flex self-end text-right sm:hidden`}>
+      <Menu>
+        <MenuButton className=" text-green-400">
+          <BiMenu size={"24px"} />
+        </MenuButton>
+        <MenuList
+          background={"black"}
+          textAlign={"right"}
+          color={"rgb(74 222 128)"}
+        >
+          <MenuItem
+            as={'a'}
+            href={"/about"}
+            className="flex justify-between"
+            icon={BiUser(Icon)}
+            background={"black"}
+          >
+            About
+          </MenuItem>
+
+          <MenuItem
+            disabled
+            as={'a'}
+            href="https://leonewport.vercel.app/privacy"
+            className="flex justify-between"
+            icon={BiSolidLock(Icon)}
+            background={"black"}
+          >
+            Legal (not ready yet calm down!!!)
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </div>
+  );
 };
