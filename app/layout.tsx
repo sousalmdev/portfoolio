a636a7417e2df3f7fdc3b46f1b6ec3faa767ffb4
sousalmdev <leandromedeiros690@gmail.com>
2024-04-04 00:59:35 -0300

    sheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,8 @@ export const ActiveLink = ({ href, children }: ActiveLinkProps) => {
 };
 
 
+
+
 export const metadata: Metadata = {
   title: "Portfolio Leandro Sousa"
 };
@@ -39,8 +41,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+<html translate="yes" className="scroll-smooth">  
     <ChakraProvider>
-      <div className="scroll-smooth">
+      <body className='bg-black flex-col h-min-screen flex text-left items-center'>
         <header className={`${jost.className} bg-black sticky top-0 p-2 bg-black z-50  text-white w-11/12 md:w-3/4 flex items-center md:justify-between justify-between `}>
           <div id="title" className="flex gap-4 py-2 items-center">
             <Image src={leo} alt="leo" height={30} className="rounded-full" />
@@ -48,6 +51,7 @@ export default function RootLayout({
           </div>
           <div>
             <MenuUi />
+
           </div>
           <nav className=" hidden sm:flex justify-end">
             <ul className="flex text-sm md:text-base gap-5 items-center">
@@ -66,13 +70,16 @@ export default function RootLayout({
             </ul>
           </nav>
         </header>
+
         {children}
+
         <footer className={`absolute bottom-0 h-24 p-4 w-full text-white flex items-center justify-center border-t border-white ${jost.className}`}>
           <ul className="flex items-center md:justify-evenly md:gap-0 justify-center text-center md:w-3/4 w-11/12 gap-7  text-sm md:text-lg">
             <li className="flex flex-col">
               <p className="font-bold">©2024</p>
               <Link href='/' className="underline"><p>Leo Sousa</p></Link>
             </li>
+
             <li className="flex flex-col"><p className="font-bold">Social</p>
               <div className="flex gap-2">
                 <p><Link className="underline" href={'https:linkedin.com/in/leosousadev'}>LinkedIn</Link></p>
@@ -83,9 +90,11 @@ export default function RootLayout({
             <li className="flex flex-col"><p className="font-bold">Legal</p>
               <Link href={'/privacy'} className="underline">Privacy</Link>
             </li>
+
           </ul>
         </footer>
-      </div>
+      </body>
     </ChakraProvider>
+</html>     
   );
 }
