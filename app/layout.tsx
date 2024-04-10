eff3ffe9bd9854facd8e1a64c13f01f47f15c5ec
sousalmdev <leandromedeiros690@gmail.com>
2024-04-10 18:46:30 -0300

    Componentizing metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Raleway } from "next/font/google";
 import "./globals.css";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
-import Head from "next/head";
+import { Metatags } from "./metatags/metatags";
 import { Suspense } from "react";
 const jost = Raleway({
   subsets: ["latin"],
@@ -26,21 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="pt" translate="yes">
-      <Head>
-        <meta name="theme-color" content="#1b2631" />
-        <meta name="description" content="My name is Leandro Medeiros de Sousa, a brazilian developer, born in Rio de Janeiro. I had my first touch with programming through the CC50 Harvard Course, first meeting C language and Programming Logic. After that, I went full on Front End Development, because I feel familiar to art and creative stuff."/>
-<meta property="og:url" content="https://portfoleosous.vercel.app/"/>
-<meta property="og:type" content="website"/>
-<meta property="og:title" content="Portfolio Leandro Sousa"/>
-<meta property="og:description" content="My name is Leandro Medeiros de Sousa, a brazilian developer, born in Rio de Janeiro. I had my first touch with programming through the CC50 Harvard Course, first meeting C language and Programming Logic. After that, I went full on Front End Development, because I feel familiar to art and creative stuff."/>
-<meta property="og:image" content="https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/portfoleosous.vercel.app/Portfolio%20Leandro%20Sousa/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Feb259247-642b-43a0-8a2d-03363dea6f0f.png%3Ftoken%3D_y5oElq8g65X7ZOuaTPm0Rw5SkONb7SLhhFUOp-um0Y%26height%3D640%26width%3D640%26expires%3D33248775339/og.png"/>
-<meta name="twitter:card" content="summary_large_image"/>
-<meta property="twitter:domain" content="portfoleosous.vercel.app"/>
-<meta property="twitter:url" content="https://portfoleosous.vercel.app/"/>
-<meta name="twitter:title" content="Portfolio Leandro Sousa"/>
-<meta name="twitter:description" content="My name is Leandro Medeiros de Sousa, a brazilian developer, born in Rio de Janeiro. I had my first touch with programming through the CC50 Harvard Course, first meeting C language and Programming Logic. After that, I went full on Front End Development, because I feel familiar to art and creative stuff."/>
-<meta name="twitter:image" content="https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/portfoleosous.vercel.app/Portfolio%20Leandro%20Sousa/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Feb259247-642b-43a0-8a2d-03363dea6f0f.png%3Ftoken%3D_y5oElq8g65X7ZOuaTPm0Rw5SkONb7SLhhFUOp-um0Y%26height%3D640%26width%3D640%26expires%3D33248775339/og.png"/>
-      </Head>
+      <Metatags />
       <ChakraProvider>
         <body
           className={`${jost.className} bg-black flex-col h-min-screen flex text-left items-center`}
