90e9d7c4104f7389bfaefdcf9308e767ecbdb718
sousalmdev <leandromedeiros690@gmail.com>
2024-05-14 00:16:17 -0300

    Code cleansing

--- app/components/carrossel.tsx
+++ app/components/carrossel.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Image from "next/image";
-import { TbBrandNextjs } from "react-icons/tb";
 import {
   Carousel,
   CarouselContent,
